2f074db86a308f95c44d986a5c8368075388ceed
EricDelaporte <eric.delaporte@build-ideas.de>
2019-02-07 00:29:55 +0100

    es 6 updates fixed missing imports removed unused vars

--- test/helpers.js
+++ test/helpers.js
@@ -16,70 +16,70 @@
 
 'use strict';
 
-var fs = require('fs');
-var _ = require('lodash');
-var util = require('util');
+const fs = require('fs');
+const _ = require('lodash');
+const util = require('util');
 
-module.exports.copyFile = function(source, target, cb) {
-  cb = _.once(cb);
+module.exports.copyFile = function (source, target, cb) {
+    cb = _.once(cb);
 
-  var rd = fs.createReadStream(source);
-  rd.on('error', function(err) {
-    cb(err);
-  });
+    const rd = fs.createReadStream(source);
+    rd.on('error', function (err) {
+        cb(err);
+    });
 
-  var wr = fs.createWriteStream(target);
-  wr.on('error', function(err) {
-    cb(err);
-  });
-  wr.on('close', function(err) {
-    cb(err);
-  });
-  rd.pipe(wr);
+    const wr = fs.createWriteStream(target);
+    wr.on('error', function (err) {
+        cb(err);
+    });
+    wr.on('close', function (err) {
+        cb(err);
+    });
+    rd.pipe(wr);
 };
 
 // intercepts stdout and stderr
 // returns object with methods:
 //   output() : returns captured string
 //   release() : must be called when done, returns captured string
-module.exports.captureOutput = function captureOutput() {
-  var old_stdout_write = process.stdout.write;
-  var old_console_error = console.error;
+module.exports.captureOutput = () => {
+    const old_stdout_write = process.stdout.write;
+    const old_console_error = console.error;
 
-  var captured = '';
-  var callback = function(string) {
-    captured += string;
-  };
-
-  process.stdout.write = (function(write) {
-    return function(string, encoding, fd) {
-      var args = _.toArray(arguments);
-      write.apply(process.stdout, args);
-
-      // only intercept the string
-      callback.call(callback, string);
+    let captured = '';
+    const callback = function (string) {
+        captured += string;
     };
-  }(process.stdout.write));
 
-  console.error = (function(log) {
-    return function() {
-      var args = _.toArray(arguments);
-      args.unshift('[ERROR]');
-      console.log.apply(console.log, args);
+    process.stdout.write = (function (write) {
+        return function (string, encoding, fd) {
+            const args = _.toArray(arguments);
+            write.apply(process.stdout, args);
 
-      // string here encapsulates all the args
-      callback.call(callback, util.format(args));
+            // only intercept the string
+            callback.call(callback, string);
+        };
+    }(process.stdout.write));
+
+    console.error = (function () {
+        return function () {
+            const args = _.toArray(arguments);
+            args.unshift('[ERROR]');
+            console.log.apply(console.log, args);
+
+            // string here encapsulates all the args
+            callback.call(callback, util.format(args));
+        };
+    }(console.error));
+
+    return {
+        output() {
+            return captured;
+        },
+        release() {
+            process.stdout.write = old_stdout_write;
+            console.error = old_console_error;
+            return captured;
+        }
     };
-  }(console.error));
-
-  return {
-    output: function output(err, reply) {
-      return captured;
-    },
-    release: function done(err, reply) {
-      process.stdout.write = old_stdout_write;
-      console.error = old_console_error;
-      return captured;
-    }
-  }
 };
